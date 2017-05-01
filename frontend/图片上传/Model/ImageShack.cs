70a27f706b53ed50c51db4bc14d675d56c528fcd
Eric <ecr23pg@gmail.com>
2017-05-01 11:31:22 +0800

    图片上传前端界面完成

---
+++ frontend/图片上传/Model/ImageShack.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace App1.Model
+{
+	public class ImageShack
+	{
+		public StorageFile File
+		{
+			set;
+			get;
+		}
+
+		/// <summary>
+		///     上传返回
+		/// </summary>
+		public string ResponseString
+		{
+			set;
+			get;
+		}
+
+		public EventHandler<string> OnUploadedEventHandler
+		{
+			set;
+			get;
+		}
+
+		/// <summary>
+		///     上传图片
+		/// </summary>
+		public async void UpLoad()
+		{
+			string url = "https://api.ocr.space/parse/image";
+			HttpClient webHttpClient = new HttpClient();
+			HttpMultipartFormDataContent httpMultipartFormDataContent =
+					new HttpMultipartFormDataContent();
+
+			var fileContent = new HttpStreamContent(await File.OpenAsync(FileAccessMode.Read));
+			fileContent.Headers.Add("Content-Type", "application/octet-stream");
+			httpMultipartFormDataContent.Add(fileContent, "file", File.Name);
+
+			var stringContent = new HttpStringContent("fb8f59d6d088957");
+
+			httpMultipartFormDataContent.Add(stringContent, "apikey");
+			var str = await webHttpClient.PostAsync(new Uri(url), httpMultipartFormDataContent);
+			JObject data = JObject.Parse(str.Content.ToString());
+			ResponseString = data["ParsedResults"][0]["ParsedText"].ToString();
+			OnUploadedEventHandler?.Invoke(this, ResponseString);
+		}
+	}
+}
---
+++ frontend/图片上传/NotifyProperty.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
+
+namespace App1
+{
+	public class NotifyProperty : INotifyPropertyChanged
+	{
+		public void UpdateProper<T>(ref T properValue, T newValue, [CallerMemberName] string properName = "")
+		{
+			if (Equals(properValue, newValue))
+			{
+				return;
+			}
+
+			properValue = newValue;
+			OnPropertyChanged(properName);
+		}
+
+		public async void OnPropertyChanged([CallerMemberName] string name = "")
+		{
+			PropertyChangedEventHandler handler = PropertyChanged;
+			await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+					() =>
+					{
+						handler?.Invoke(this, new PropertyChangedEventArgs(name));
+					});
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+	}
+}
